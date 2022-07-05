b9e159c9b71bf422f537acba623823834fec7a8d
Minh Nguyens <minhnguyen@wee.vn>
2022-07-05 11:21:37 +0700

    delete unused in utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,9 @@
 // Utils provides different function for handling errors, helper functions
 const catchAsync = require('./catchAsync');
-const AppError = require('../services/response/AppError');
 const { dbConnection, dbDisconnect } = require('./dbHandler');
-const Error = require('../config/constant/Error');
-const Success = require('../services/response/Success');
 
 module.exports = {
   catchAsync,
-  AppError,
-  Error,
-  Success,
   dbConnection,
   dbDisconnect,
 };
